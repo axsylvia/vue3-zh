7cf1429a74af2c697a3b869542661d83db7193a5
yuki <771634986@qq.com>
2021-07-27 19:07:28 +0800

    表单:modelvalue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <column-list :list="list"></column-list>
+    <!-- <column-list :list="list"></column-list> -->
+    <div class="mb-3">
+      <label class="form-label">邮箱地址</label>
+      <validate-input
+        :rules="emailRules"
+        v-model="emailVal"
+        placeholder="请输入邮箱地址"
+        type="text"
+      ></validate-input>
+      {{ emailVal }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent,ref } from 'vue';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList ,{ ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
 const currentUser: UserProps = {
  isLogin: true,
  name: 'asaki'
@@ -40,17 +51,23 @@ const testData: ColumnProps[] = [
     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
 ]
-
 export default defineComponent({
   name: 'App',
   components: {
     ColumnList,
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput
   },
   setup() {
+    const emailVal = ref('asaki')
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+
     return {
       list: testData,
-      currentUser
+      currentUser,emailRules,emailVal
     }
   }
 });
